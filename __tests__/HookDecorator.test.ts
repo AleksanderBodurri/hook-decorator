4ef90e2d7c8bdd8b458297af7d4610e3975a9e94
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2020-04-10 11:56:01 -0400

    tests: update test for new features

--- __tests__/HookDecorator.test.ts
+++ __tests__/HookDecorator.test.ts
@@ -1,21 +1,33 @@
 import { Hook } from '../src';
 
-const consoleOutput: string[] = [];
+let consoleOutput: string[] = [];
 
 @Hook({
-  beforeAction: () => consoleOutput.push('hello'),
-  afterAction: () => consoleOutput.push('!'),
+  beforeAction: {
+    callback: () => consoleOutput.push('hello'),
+    only: ['log'],
+  },
+  afterAction: {
+    callback: () => consoleOutput.push('alone'),
+    except: ['log'],
+  },
 })
 class Logger {
   log(s: string): void {
     consoleOutput.push(s);
   }
+
+  clear(): void {
+    consoleOutput = [];
+  }
 }
 
 describe('Hook', () => {
-  it('should output hello world', () => {
+  it('should fire before callback only on calling log method', () => {
     const logger = new Logger();
     logger.log('world');
-    expect(consoleOutput).toEqual(['hello', 'world', '!']);
+    expect(consoleOutput).toEqual(['hello', 'world']);
+    logger.clear();
+    expect(consoleOutput).toEqual(['alone']);
   });
 });
