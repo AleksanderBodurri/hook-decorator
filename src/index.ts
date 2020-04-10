0e38e84b389cc3564618ce5b87f82e163238591a
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2020-04-09 23:28:39 -0400

    feat: create hook decorator with basic beforeAction and afterAction functionality; include option to patch angular lifecycles

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,101 @@
-const Hook = (config: {
-    beforeAction: (...args: any[]) => any,
-    options?: {
-        patchAngular: boolean
-    }
-}) => {
-    return (constructor: any) => {
-        console.log({constructor});
-        Object.getOwnPropertyNames(constructor.prototype).filter(key => key !== 'constructor').forEach(key => {
-            if (key === 'ngOnInit') {
-                const originalAngular = constructor.ɵcmp.onInit;
-                constructor.ɵcmp.onInit = function(): any {
-                    config.beforeAction();
-                    return originalAngular.apply(this, arguments);
-                };
-            }
+type ClassDecorator = (constructor: any) => void;
+type Callback = () => any;
 
-            const original = constructor.prototype[key];
-            constructor.prototype[key] = function(): any {
-                config.beforeAction();
-                return original.apply(this, arguments);
-            };
-        });
-    };
+interface HookConfigOptions {
+  patchAngular: boolean;
+}
+
+export interface HookConfig {
+  beforeAction?: Callback;
+  afterAction?: Callback;
+  options?: HookConfigOptions;
+}
+
+const angularLifeCycleInternalMappings: { [key: string]: string } = {
+  ngOnChanges: 'onChanges',
+  ngOnInit: 'onInit',
+  ngDoCheck: 'doCheck',
+  ngAfterContentInit: 'afterContentInit',
+  ngAfterContentChecked: 'afterContentChecked',
+  ngAfterViewInit: 'afterViewInit',
+  ngAfterViewChecked: 'afterViewChecked',
+  ngOnDestroy: 'onDestroy',
+};
+
+export const Hook = (config: HookConfig): ClassDecorator => {
+  const { beforeAction, afterAction } = config;
+  return (constructor) => {
+    if (!beforeAction && !afterAction) {
+      return;
+    }
+    Object.getOwnPropertyNames(constructor.prototype)
+      .filter((key) => key !== 'constructor')
+      .forEach((key) => assertFunction(constructor.prototype[key]) && applyHook(constructor, key, config));
+  };
+};
+
+const assertFunction = (value: any) => {
+  return typeof value === 'function';
+};
+
+const applyHook = (constructor: any, key: string, config: HookConfig) => {
+  const { options } = config;
+
+  parseAndPatch(constructor.prototype, key, config);
+
+  if (options?.patchAngular && constructor.ɵcmp && angularLifeCycleInternalMappings[key]) {
+    parseAndPatchAngular(constructor.ɵcmp, angularLifeCycleInternalMappings[key], config);
+  }
+};
+
+const parseAndPatch = (functionToPatchParent: Function, keyToFunction: string, config: HookConfig) => {
+  const { beforeAction, afterAction } = config;
+
+  if (beforeAction && afterAction) {
+    patchWithBeforeAndAfter(functionToPatchParent, keyToFunction, beforeAction, afterAction);
+  } else if (beforeAction) {
+    patchWithBefore(functionToPatchParent, keyToFunction, beforeAction);
+  } else if (afterAction) {
+    patchWithAfter(functionToPatchParent, keyToFunction, afterAction);
+  }
+};
+
+const parseAndPatchAngular = (componentMetadata: any, lifecycle: string, config: HookConfig) => {
+  const lifecycleToPatch = componentMetadata[lifecycle];
+  if (!lifecycleToPatch) {
+    return;
+  }
+
+  parseAndPatch(componentMetadata, lifecycle, config);
+};
+
+const patchWithBefore = (functionToPatchParent: Function, keyToFunction: string, before: Callback) => {
+  const original = functionToPatchParent[keyToFunction];
+  functionToPatchParent[keyToFunction] = function (): any {
+    before.call(this);
+    return original.apply(this, arguments);
+  };
+};
+
+const patchWithAfter = (functionToPatchParent: Function, keyToFunction: string, after: Callback) => {
+  const original = functionToPatchParent[keyToFunction];
+  functionToPatchParent[keyToFunction] = function (): any {
+    const result = original.apply(this, arguments);
+    after.call(this);
+    return result;
+  };
+};
+
+const patchWithBeforeAndAfter = (
+  functionToPatchParent: Function,
+  keyToFunction: string,
+  before: Callback,
+  after: Callback
+) => {
+  const original = functionToPatchParent[keyToFunction];
+  functionToPatchParent[keyToFunction] = function (): any {
+    const result = original.apply(this, arguments);
+    after.call(this);
+    return result;
+  };
 };
